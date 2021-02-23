721324f94b953fdf58e5337a0bd43b3ac60b39bf
Felix Ortiz <fo2016@nyu.edu>
2021-02-23 14:22:31 -0700

    Initial commit
    Signed-off-by: Felix Ortiz <fo2016@nyu.edu>

--- solution.py
+++ solution.py
@@ -1,50 +1,80 @@
-#import socket module
 from socket import *
-import sys # In order to terminate the program
 
-def webServer(port=13331):
-    serverSocket = socket(AF_INET, SOCK_STREAM)
 
-    #Prepare a sever socket
-    serverSocket.bind(("", port))
-    #Fill in start
-    serverSocket.listen()
-    #Fill in end
+def smtp_client(port=1025, mailserver='127.0.0.1'):
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
 
-    while True:
-        #Establish the connection
-        #print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept()
-        try:
-            message = connectionSocket.recv(2048).decode()
-            filename = message.split()[1]
-            f = open(filename[1:])
-            outputdata =  f.read()
+    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
 
-            #Send one HTTP header line into socket
-            #Fill in start
-            connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".encode())
-            #Fill in end
+    # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
-            #Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
+    # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
 
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.close()
-        except IOError:
-            #Send response message for file not found (404)
-            #Fill in start
-            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-            #Fill in end
+    # Fill in end
 
-            #Close client socket
-            #Fill in start
-            connectionSocket.close()
-            #Fill in end
+    recv = clientSocket.recv(1024).decode()
+    print(recv)
 
-    serverSocket.close()
-    sys.exit()  # Terminate the program after sending the corresponding data
+    if recv[:3] != '220':
+        print('220 reply not received from server.')
 
-if __name__ == "__main__":
-    webServer(13331)
+    # Send HELO command and print server response.
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
+
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
+
+    # Send MAIL FROM command and print server response.
+    # Fill in start
+    mailfrom = "MAIL FROM: <fantortiz@gmail.com>\r\n"
+    clientSocket.send(mailfrom.encode())
+    recv2 = clientSocket.recv(1024).decode()
+    print(recv2)
+    # Fill in end
+
+    # Send RCPT TO command and print server response.
+    # Fill in start
+    mailto = "RCPT TO: <felix.ortiz@faosystems.com>\r\n"
+    clientSocket.send(mailto.encode())
+    recv3 = clientSocket.recv(1024).decode()
+    print(recv3)
+    # Fill in end
+
+    # Send DATA command and print server response.
+    # Fill in start
+    data = "DATA\r\n"
+    clientSocket.send(data.encode())
+    recv4 = clientSocket.recv(1024).decode()
+    print(recv4)
+    # Fill in end
+
+    # Send message data.
+    # Fill in start
+    msg = "Hello, this is a reminder that your vacation date is fast approaching.\r\n"
+    clientSocket.send(msg.encode())
+    # Fill in end
+
+    # Message ends with a single period.
+    # Fill in start
+    msg = "\r\n.\r\n"
+    clientSocket.send(msg.encode())
+    recv5 = clientSocket.recv(1024).decode()
+    print(recv5)
+    # Fill in end
+
+    # Send QUIT command and get server response.
+    # Fill in start
+    msg = "QUIT\r\n"
+    clientSocket.send(msg.encode())
+    recv6 = clientSocket.recv(1024).decode()
+    print(recv6)
+    # Fill in end
+
+
+if __name__ == '__main__':
+    smtp_client(1025, '127.0.0.1')
